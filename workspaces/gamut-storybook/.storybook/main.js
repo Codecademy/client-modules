3d561a4bb9d90fd9a9968e2e179f3f3af76ae2b5
Jake Hiller <jakemhiller@gmail.com>
2020-01-27 15:41:46 -0500

    feat: Remove storybook package aliases (#610)
    * Remove package aliasing in storybook
    * Move gamut-storybook to `packages/styleguide`

--- workspaces/gamut-storybook/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.(mdx|js)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-actions',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-  ],
-};
