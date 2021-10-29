e14bbe080151b1f043c174076f73f09cd713bd8a
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-10-29 15:00:19 -0400

    chore: finish removing deploy scripts (#3)
    * chore: finish removing deploy scripts
    
    * run npm whoami
    
    * Move format:verify and lint to GHA
    
    * How about context: org-global
    
    * Fix lint
    
    * Remove designs from PR template too
    
    * Remove org-global
    
    * Use github-publishing context
    
    * Context name: GitHub Publishing

--- packages/eslint-config/fix.js
+++ packages/eslint-config/fix.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['eslint:recommended'],
-  plugins: ['local-rules', 'react', 'react-hooks', 'simple-import-sort'],
+  plugins: ['react', 'react-hooks', 'simple-import-sort'],
   rules: {
-    'local-rules/prefer-use-selector-with': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'simple-import-sort/imports': 'error',
