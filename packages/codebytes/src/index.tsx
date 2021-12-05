d96d1a789d35ac84acc53561c44446ce681e898f
Bana Malik <bana@codecademy.com>
2021-12-05 17:20:56 -0500

    remove rendermode

--- packages/codebytes/src/index.tsx
+++ packages/codebytes/src/index.tsx
@@ -4,25 +4,18 @@ import { Editor } from './editor';
 
 export interface CodeByteEditorProps {
   text: string;
-  renderMode: boolean;
 }
 
 export const CodeByteEditor: React.FC<CodeByteEditorProps> = ({
   text: initialText,
-  renderMode,
 }) => {
   const [text, setText] = useState<string>(initialText);
-
-  const [hasBeenEdited, setHasBeenEdited] = useState(false);
   return (
     <>
       <Editor
         text={text}
         onChange={(newText: string) => {
           setText(newText);
-          if (!renderMode && hasBeenEdited === false) {
-            setHasBeenEdited(true);
-          }
         }}
       />
     </>
