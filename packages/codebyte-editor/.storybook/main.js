b89937810c8a2da05f207ae97fb354fd73b43685
Bana Malik <bana@codecademy.com>
2021-12-14 16:37:33 -0500

    update main.js

--- packages/codebyte-editor/.storybook/main.js
+++ packages/codebyte-editor/.storybook/main.js
@@ -1,5 +1,51 @@
+const path = require('path');
+const { configs } = require('@codecademy/webpack-config');
+
+// https://github.com/storybookjs/storybook/issues/12262#issuecomment-681953346
+// make a shallow copy of an object, rejecting keys that match /emotion/
+function emotionless(object) {
+  let result = {};
+  for (let key in object) {
+    if (!/emotion/.test(key)) {
+      result[key] = object[key];
+    }
+  }
+  return result;
+}
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    // './addons/system/preset',
+    // 'storybook-addon-designs',
+  ],
+  // stories: ['../src/stories/**/*.stories.(mdx|tsx)'],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+
+  webpackFinal: (config) => {
+    config.module.rules = config.module.rules.concat(
+      configs.css().module.rules
+    );
+
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...emotionless(config.resolve.alias)
+      },
+    };
+
+    return config;
+  },
 };
