675361b86eb5fd9b7605c6adfe0069de2b426ef5
Bana Malik <bana@codecademy.com>
2022-01-03 18:40:01 -0500

    fix prettier issues

--- packages/codebytes/src/api.ts
+++ packages/codebytes/src/api.ts
@@ -1,25 +1,28 @@
 import type { languageOption } from './consts';
 
-  interface Response {
-    stderr: string;
-    stdout: string;
-    exit_code: number;
-  }
-  
-  interface PostSnippetData {
-    language: languageOption;
-    code: string;
-  }
+interface Response {
+  stderr: string;
+  stdout: string;
+  exit_code: number;
+}
 
-  export const postSnippet = async (data: PostSnippetData, snippetsBaseUrl?: string): Promise<Response> => {
-    const snippetsEndpoint = `https://${snippetsBaseUrl}/snippets`;
+interface PostSnippetData {
+  language: languageOption;
+  code: string;
+}
 
-    const response = await fetch(snippetsEndpoint, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'x-codecademy-user-id': 'codebytes-anon-user',
-      },
-    });
-    return response.json();
-  };
+export const postSnippet = async (
+  data: PostSnippetData,
+  snippetsBaseUrl?: string
+): Promise<Response> => {
+  const snippetsEndpoint = `https://${snippetsBaseUrl}/snippets`;
+
+  const response = await fetch(snippetsEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'x-codecademy-user-id': 'codebytes-anon-user',
+    },
+  });
+  return response.json();
+};
