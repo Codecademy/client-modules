9c15b4be2b1b537297a9fb3dae7d873b5ad264c2
Bana Malik <bana@codecademy.com>
2022-01-12 22:12:34 -0500

    add configuration for visual packages

--- jest.config.base.js
+++ jest.config.base.js
@@ -17,6 +17,11 @@ module.exports = (packageName) => ({
   snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleDirectories: ['node_modules'],
   collectCoverageFrom: ['<rootDir>/**/*.{js,jsx,ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/../../script/jest/fileMock',
+    '\\.(css|scss)$': '<rootDir>/../../script/jest/styleMock',
+  },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/stories/',
