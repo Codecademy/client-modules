4ac9d1c5c1d1c82089b8a7fbb5eb79f5f3ef5601
Hailey <hailey@codecademy.com>
2022-01-26 12:51:50 -0500

    Added TODO and changed base url to point to either prod or staging based on node env

--- packages/codebytes/src/libs/eventTracking.ts
+++ packages/codebytes/src/libs/eventTracking.ts
@@ -1,8 +1,11 @@
 import { createTracker } from '@codecademy/tracking';
 
 const IS_DEV = process.env.NODE_ENV === 'development';
+// TODO: confirm tracking details and implementation DISC-447
 const API_BASE_URL =
-  process.env.GATSBY_MONOLITH_API_BASE || 'https://www.codecademy.com';
+  process.env.NODE_ENV === 'production'
+    ? 'https://www.codecademy.com'
+    : 'https://staging.codecademy.com/';
 
 const tracker = createTracker({
   apiBaseUrl: API_BASE_URL,
