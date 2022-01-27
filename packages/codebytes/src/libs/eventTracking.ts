a423a85de93bdcbebf660d6e0442516fc75ece6d
Hailey <hailey@codecademy.com>
2022-01-27 10:18:52 -0500

    Add comment descriptor

--- packages/codebytes/src/libs/eventTracking.ts
+++ packages/codebytes/src/libs/eventTracking.ts
@@ -3,6 +3,7 @@ import { createTracker } from '@codecademy/tracking';
 const IS_DEV = process.env.NODE_ENV === 'development';
 
 // TODO: confirm tracking details and implementation DISC-447
+// the window.location.origin will match what the iframe url is which in forums is https://www.codecademy.com
 const tracker = createTracker({
   apiBaseUrl:
     typeof window === undefined
