3b18df4678858d633fd96e2d7f48f1a9d1fea032
Bana Malik <bana@codecademy.com>
2022-02-01 18:57:25 -0500

    chore: some package touchups

--- packages/codebytes/src/libs/eventTracking.ts
+++ packages/codebytes/src/libs/eventTracking.ts
@@ -5,7 +5,7 @@ const IS_DEV = process.env.NODE_ENV === 'development';
 // TODO: confirm tracking details and implementation DISC-447
 const tracker = createTracker({
   apiBaseUrl:
-    typeof window === undefined
+    typeof window === 'undefined'
       ? 'https://www.codecademy.com'
       : window.location.origin,
   verbose: IS_DEV,
