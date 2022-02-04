d35b98a9f0c2a31dd2c7d80ffcf69bd40be03b76
Rebecca Borison <borisonr@gmail.com>
2022-02-04 14:00:28 -0500

    holy grail

--- packages/codebytes/src/MonacoEditor/index.tsx
+++ packages/codebytes/src/MonacoEditor/index.tsx
@@ -3,13 +3,16 @@
 // We are working on a monaco package in client-modules that has more configuration around themes and languages
 // Monaco as a shared package RFC https://www.notion.so/codecademy/Monaco-editor-as-a-shared-package-1f4484db165b4abc8394c3556451ef6a
 
-import ReactMonacoEditor, { OnMount } from '@monaco-editor/react';
+import loadable from '@loadable/component';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import React, { useCallback, useRef } from 'react';
+import MonacoEditor from 'react-monaco-editor';
 import ResizeObserver from 'react-resize-observer';
 
 import { dark } from './theme';
 import { Monaco } from './types';
+
+const ReactMonacoEditor = loadable(() => import('react-monaco-editor'));
 
 export type SimpleMonacoEditorProps = {
   value: string;
@@ -17,22 +20,19 @@ export type SimpleMonacoEditorProps = {
   onChange?: (value: string) => void;
 };
 
-type ThemedEditor = Parameters<OnMount>[0];
+// type ThemedEditor = Parameters<OnMount>[0];
 
 export const SimpleMonacoEditor: React.FC<SimpleMonacoEditorProps> = ({
   value,
   language,
   onChange,
 }) => {
-  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-  const editorWillMount = useCallback(
-    (editor: ThemedEditor, monaco: Monaco) => {
-      editorRef.current = editor;
-      monaco.editor.defineTheme('dark', dark);
-      monaco.editor.setTheme('dark');
-    },
-    []
-  );
+  // const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const editorRef = useRef<MonacoEditor>(null);
+  const editorWillMount = useCallback((monaco: Monaco) => {
+    monaco.editor.defineTheme('dark', dark);
+    monaco.editor.setTheme('dark');
+  }, []);
   return (
     <>
       <ResizeObserver
@@ -50,6 +50,7 @@ export const SimpleMonacoEditor: React.FC<SimpleMonacoEditorProps> = ({
         theme="vs-dark"
         value={value}
         language={language}
+        ref={editorRef}
       />
     </>
   );
