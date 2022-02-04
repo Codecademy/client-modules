4be5c54a4c23cccbf42a9ceb161bea884f0baea6
Rebecca Borison <borisonr@gmail.com>
2022-02-04 16:17:13 -0500

    hail mary

--- packages/codebytes/src/MonacoEditor/index.tsx
+++ packages/codebytes/src/MonacoEditor/index.tsx
@@ -3,6 +3,8 @@
 // We are working on a monaco package in client-modules that has more configuration around themes and languages
 // Monaco as a shared package RFC https://www.notion.so/codecademy/Monaco-editor-as-a-shared-package-1f4484db165b4abc8394c3556451ef6a
 
+import { Box } from '@codecademy/gamut';
+import styled from '@emotion/styled/types/base';
 import ReactMonacoEditor, { OnMount } from '@monaco-editor/react';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import React, { useCallback, useRef } from 'react';
@@ -19,6 +21,20 @@ export type SimpleMonacoEditorProps = {
 
 type ThemedEditor = Parameters<OnMount>[0];
 
+// A hidden element in monaco was causing its container to overflow
+// https://github.com/microsoft/monaco-editor/issues/29#issuecomment-753854541
+const EditorWrapper = styled(Box)`
+  & .monaco-editor.rename-box,
+  .monaco-hover {
+    top: 0;
+  }
+  // A hidden overflowing element causes accessibility errors
+  // The background of the overflowing element causes triggers color contrast errors
+  & .lines-content {
+    height: 100% !important;
+  }
+`;
+
 export const SimpleMonacoEditor: React.FC<SimpleMonacoEditorProps> = ({
   value,
   language,
@@ -34,7 +50,7 @@ export const SimpleMonacoEditor: React.FC<SimpleMonacoEditorProps> = ({
     []
   );
   return (
-    <>
+    <EditorWrapper>
       <ResizeObserver
         onResize={({ height, width }) => {
           editorRef.current?.layout({
@@ -51,6 +67,6 @@ export const SimpleMonacoEditor: React.FC<SimpleMonacoEditorProps> = ({
         value={value}
         language={language}
       />
-    </>
+    </EditorWrapper>
   );
 };
