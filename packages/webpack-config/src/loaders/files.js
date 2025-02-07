30c977a53d08946d376c8aed899b176baad68fbb
Jake Hiller <jakemhiller@gmail.com>
2025-02-07 16:04:13 -0500

    made a change

--- packages/webpack-config/src/loaders/files.js
+++ packages/webpack-config/src/loaders/files.js
@@ -1,7 +1,7 @@
 const files = {
   default: {
     loader: 'file-loader',
-    test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff2?$|\.ttf$|\.eot|\.wav$|\.mp3$/,
+    test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff2?$|\.ttf$|\.eot|\.wav$|\.mp3$|\.ogg$/,
   },
 };
 
