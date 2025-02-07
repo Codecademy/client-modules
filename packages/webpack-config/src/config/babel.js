ff662b445dfca567556988b3b4f85370f201312a
dreamwasp <cass.spain@skillsoft.com>
2025-02-07 15:29:05 -0500

    minimize changes, maybe try to fix PR workflow l8r

--- packages/webpack-config/src/config/babel.js
+++ packages/webpack-config/src/config/babel.js
@@ -5,7 +5,7 @@ const babelConfig = (options) => ({
     rules: [
       merge(
         {
-          test: /\.(mj|m|j|t)sx?$/,
+          test: /\.(mj|j|t)sx?$/,
           loader: 'babel-loader',
           options: {
             cacheDirectory: true,
